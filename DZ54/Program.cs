a3a38780e8483028c163a7c662e2b9f9321f7a5e
Maxim <m.krishnevskiy@kmg.kz>
2022-07-20 17:20:20 +0600

    DZ54

---
+++ DZ54/Program.cs
@@ -0,0 +1,60 @@
+﻿//  Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+
+int[,] CreateMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            //int x = Convert.ToInt32(Console.ReadLine());
+            int x = new Random().Next(1, 100);
+            matrix[i, j] = x;
+        }
+    }
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+
+    return matrix;
+}
+
+void AvgMatrix(int[,] matrix)
+{
+    Console.WriteLine();
+    int x;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int f = j + 1; f < matrix.GetLength(1); f++)
+            {
+                if (matrix[i, f] < matrix[i, j])
+                {
+                    x = matrix[i, j];
+                    matrix[i, j] = matrix[i, f];
+                    matrix[i, f] = x;
+
+                }
+
+            }
+        }
+    }
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(0); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = new int[4, 4];
+matrix = CreateMatrix(matrix);
+AvgMatrix(matrix);
